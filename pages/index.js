df63639afc06029e1261d3bb93ec46c2c0b58839
knitron07 <samarmahur1992@gmail.com>
2021-12-09 01:43:15 +0530

    large card and footer

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import SmallCard from "../components/SmallCard";
 import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
 export default function Home({ exploreData, cardsData }) {
   return (
@@ -39,7 +41,15 @@ export default function Home({ exploreData, cardsData }) {
             ))}
           </div>
         </section>
+
+        <LargeCard
+          src="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb"
+          buttonText="Get Inspired"
+        />
       </main>
+      <Footer />
     </div>
   );
 }
