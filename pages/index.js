0da5aeed7a8d2faa7992c98864ed96f616869c17
knitron07 <samarmahur1992@gmail.com>
2021-12-08 19:02:57 +0530

    banner and smallCard

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
 
-export default function Home() {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -9,6 +12,48 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section>
+          <div className="pt-6">
+            <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {exploreData?.map(({ img, distance, location }) => {
+                console.log(img, location, distance);
+                return (
+                  <SmallCard
+                    key={img}
+                    img={img}
+                    distance={distance}
+                    location={location}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          {cardsData?.map(({ img, title }) => (
+            <MediumCard key={img} img={img} title={title} />
+          ))}
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+  const cardsData = await fetch("https://links.papareact.com/zpl").then((res) =>
+    res.json()
+  );
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
 }
